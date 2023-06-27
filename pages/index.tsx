ccbab2fe4cb7541754757e0a4c3176b778522b2b
malizkl <63977569+malizkl@users.noreply.github.com>
2023-06-27 20:59:46 +0300

    image any

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ function cn(...classes: string[]) {
 }
 
 
-export default function Gallery({ images }) {
+export default function Gallery({ images }: { images: any[] }) {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 
